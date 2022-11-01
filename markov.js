063754eb4eb24f689be3f350ee73b85115073185
Jennifer Reuting <jen@reuting.com>
2022-11-01 14:32:46 -0700

    initial commit

---
+++ markov.js
@@ -0,0 +1,44 @@
+/** Textual markov chain generator. */
+
+
+class MarkovMachine {
+
+  /** Build markov machine; read in text.*/
+
+  constructor(text) {
+    // A "word" will also include any punctuation around the word, so this will
+    // include things like "The", "cat", "cat.".
+    this.words = text.split(/[ \r\n]+/);
+    this.chains = this.getChains();
+  }
+
+  /** Get markov chain: returns Map of Markov chains.
+   *
+   *  For text of "The cat in the hat.", chains will be:
+   * 
+   *  {
+   *   "The": ["cat"],
+   *   "cat": ["in"],
+   *   "in": ["the"],
+   *   "the": ["hat."],
+   *   "hat.": [null],
+   *  }
+   * 
+   * */
+
+  getChains() {
+    // TODO: implement this!
+  }
+
+
+  /** Return random text from chains, starting at the first word and continuing
+   *  until it hits a null choice. */
+
+  getText() {
+    // TODO: implement this!
+
+    // - start at the first word in the input text
+    // - find a random word from the following-words of that
+    // - repeat until reaching the terminal null
+  }
+}
